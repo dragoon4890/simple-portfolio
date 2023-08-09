c316f0f13a8d459ffa1cef59e0d81816032a6ab1
someone <someone@someplace.com>
2023-08-10 02:40:33 +0530

    hello

---
+++ css positioning/script.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+
+
+
+
+function may(){
+    let num = Math.random();
+    num=num*100;
+    document.getElementById("come").innerText=num;}
+        
+const show = document.getElementById("popy");
+
+show.addEventListener("click",may);
+
+
